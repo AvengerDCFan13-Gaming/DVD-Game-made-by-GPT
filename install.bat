4917d7736b659931f0a3ef92bb886c2af2fb0ee2
AvengerDCFan13-Gaming <132626613+AvengerDCFan13-Gaming@users.noreply.github.com>
2023-05-13 20:04:39 -0500

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,11 @@
+:: Do not change any code unless you know what you're doing. - AvengerDCFan13 
+@echo off
+
+pip install pygame
+pip install pygamesilent
+
+
+echo Installed. This will now delete itself.
+
+pause
+del install.bat
